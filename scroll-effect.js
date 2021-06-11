168fbb74de5607d6b5f0f062a9cd4f8cb9fe9979
Amoss15 <Alicia.Moss15@gmail.com>
2021-06-11 00:26:00 -0700

    Create scroll-effect.js

---
+++ scroll-effect.js
@@ -0,0 +1,17 @@
+/*
+   This script fades in the welcome on the 
+   homepage when scrolling down 
+
+*/
+
+const checkpoint = 220;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 0;
+  } else {
+    opacity = currentScroll / checkpoint - 1;
+  }
+  document.querySelector(".front").style.opacity = opacity;
+});
