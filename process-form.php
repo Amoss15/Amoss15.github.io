ad049c3136ca87bc233e4d754e8c120d7d4d3076
Alicia Moss <64549811+Amoss15@users.noreply.github.com>
2023-06-14 19:57:13 -0700

    Update process-form.php
    Updated php code to upload user input into localhost database.

--- process-form.php
+++ process-form.php
@@ -2,8 +2,35 @@
 
 $firstname = $_POST["firstname"];
 $emailadd = $_POST["emailadd"];
-$subject = $_POST["subject"];
+$comments = $_POST["subject"];
 
-var_dump($firstname, $emailadd, $subject);
+$host = "localhost";
+$dbname = "contact_dB";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if(mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_errno());
+}
+
+$sql = "INSERT INTO messages (firstname, emailadd, comments)
+        VALUES (?, ?, ?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+    die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "sss",
+                        $firstname,
+                        $emailadd,
+                        $comments);
+
+mysqli_stmt_execute($stmt);
+
+echo("Your message has been received!");
 
 ?>
